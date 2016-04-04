00801f143ad1114cc79fb0a6619b19d8e299ebe2
Lusindiso <lusindiso@projectcodex.co>
2016-04-04 16:33:27 +0200

    lusindiso

--- Anging_frinds.js
+++ Anging_frinds.js
@@ -2,31 +2,40 @@ var friends = "Xola is 23, Peter is 18, Oya is 29, Ellie is 20, Sive is 27, Alut
 
 
 var myFriends = friends.split(",");
-  // console.log(myFriends);
+// console.log(myFriends);
 
 
- pArray = [];
+pArray = [];
 
 //looping through and removing the is in my array
-  myFriends.forEach(function(ab){
-     pArray.push(ab.split("is"));
-   });
+myFriends.forEach(function(ab) {
+  pArray.push(ab.split("is"));
+});
 
-      //  console.log(pArray);
+//  console.log(pArray);
 
-  var xArray = [];
+var xArray = [];
 
 
-pArray.forEach(function(ac){
-   if( ac[1] < 20){ //fnding the youngest
-  xArray.push({Name:ac[0], Age:Number(ac[1])})
-}
- });
-console.log(xArray);
 
-// var nArray = [];
-//
-// xArray.forEach(function(ad){
-//   nArray.push(ad[1]);
-// });
-//  console.log(nArray);
+var nArray = [];
+//finding friends younger than Ellie
+pArray.forEach(function(ad) {
+
+  nArray.push({
+    Name: ad[0],
+    Age: Number(ad[1])
+  })
+
+});
+console.log(nArray)
+
+pArray.forEach(function(ac) {
+  if (ac[1] < 20) { //fnding the youngest
+    xArray.push({
+      Name: ac[0],
+      Age: Number(ac[1])
+    })
+  }
+});
+console.log(xArray, "are younger");
