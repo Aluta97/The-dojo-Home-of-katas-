050c8f4467ffa5a3958b5a5385edd4486c423a53
Lusindiso <lusindiso@projectcodex.co>
2016-04-13 10:42:52 +0200

    another commit

--- Trafficlight_Dash.js
+++ Trafficlight_Dash.js
@@ -1,4 +1,28 @@
-var lights = "There are 9 traffic lights on my way to work. On my way in 3 were red, 2 were green".
+  var lights = "There are 9 trafficlights on my way to work. On my way in 3 were red, 2 were green";
 
-//store your lights trafficLights in a variable.
-//to get the orange light you have to take all lights and subtract the green and red.
+var lights = lights
+.replace("There are ","")
+.replace(" on my way to work","")
+.replace(". On my way in ",",")
+.replace(", ", ",")
+.replace(/ were/g, "")
+.split(",");
+console.log(lights);
+
+
+x = [];
+lights.forEach(function(az){
+  var parts = az.split(" ");
+    console.log(parts);
+   x.push({
+      color:parts[1],
+      light:Number(parts[0])
+    });
+});
+
+console.log(x);
+
+x.forEach(function(df){
+  console.log(df.light);
+  
+});
